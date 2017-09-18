b91e94e7f412a9de04c0a5199ed25ed048471fda
TheSpyGeek <mathieurire@gmail.com>
2017-09-18 18:52:32 +0200

    [CSE] question7 + debut question 8

--- Semester1/CSE/TP1/question6.c
+++ Semester1/CSE/TP1/question6.c
@@ -5,32 +5,31 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <time.h>
 
 #define NMAX 1024
 
-#define N 5
+#define N 3
 
 
 int main(int argc, char **argv){
 
 	int i;
 
-	// int n;
 
 	// pid_t initial_proc = getpid();
 
 	int tube[2];
 
-	// char buf[NMAX];
-	char msg[] = "bonjour";
+	long int number;
 
 
 	for(i=0; i<N; i++){
 
 		if(pipe(tube) == -1){
-			perror(NULL);
+			perror("Pipe");
 			exit(1);
-		}
+		}		
 
 		switch(fork()){
 			case -1: 
@@ -40,27 +39,26 @@ int main(int argc, char **argv){
 			case 0: // fils
 				close(tube[1]);
 
-				read(tube[0], msg, strlen(msg));
+				read(tube[0], &number, sizeof(long int));
 
-
-
-				printf("FILS: message recu %s pid %d\n", msg, getpid());
-
+				printf("valeur recue %li pid %d\n", number, getpid());
 
 				close(tube[0]);
 				break;
 			default:
 				close(tube[0]);
-
-				write(tube[1], msg, strlen(msg));
-
-				printf("PERE: message envoye %s pid %d\n", msg, getpid());
-
+				srandom (getpid());
+				number = random()%2000;
+				write(tube[1], &number, sizeof(long int));
+				printf("valeur envoyee %li pid %d\n", number, getpid());
+				close(tube[1]);
 				exit(0);
 		}
 
 	}
 
+
+	exit(0);
 	
 
 }

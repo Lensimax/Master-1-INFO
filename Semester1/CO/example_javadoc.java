7f3939b099fcc3cf5c64f9ab6c028dc22c536fdc
TheSpyGeek <mathieurire@gmail.com>
2017-11-27 15:34:32 +0100

    [CO] example de javadoc

---
+++ Semester1/CO/example_javadoc.java
@@ -0,0 +1,15 @@
+
+/**** Example de javadoc  ****/
+
+/**
+* @author Maxime Isnel 
+* @param p est un entier
+* @param max on va boucler dessus
+* @return info sur le return
+* @since version SDK/JDK
+* @exception doc sur l'exeception lancé
+*/
+
+class Test {
+
+}

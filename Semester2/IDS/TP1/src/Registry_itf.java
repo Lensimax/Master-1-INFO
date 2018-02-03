ecc499690b0cbf39a39ebe4dbdd4dc3b3a20b685
TheSpyGeek <mathieurire@gmail.com>
2018-02-03 11:21:49 +0100

    suppression de IDS qui est dans un autre dépot

--- Semester2/IDS/TP1/src/Registry_itf.java
+++
@@ -1,6 +0,0 @@
-interface Registry_itf {
-    public void add(Person p);
-    public String getPhone(String name);
-    public Iterable<Person> getAll();
-    public Person search(String name);
-}
--- Semester2/IDS/TP2/src/Hello.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.*;
-
-public interface Hello extends Remote {
-    public String sayHello()  throws RemoteException;
-    public String sayName(String name) throws RemoteException;
-}
